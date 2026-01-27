988c37d5d9f092312fbb8bf0042c605c11cc4b98
Dima Menetro <kinoru11@gmail.com>
2026-01-27 12:46:29 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,4 +1,17 @@
 export const pagesConfig = {
-	Pages: {},
+	Pages: {
+		"NewQuery": {
+			route: "/new-query",
+			title: "New Query"
+		},
+		"Results": {
+			route: "/results",
+			title: "Results"
+		},
+		"History": {
+			route: "/history",
+			title: "History"
+		}
+	},
 	mainPage: "NewQuery"
 }
