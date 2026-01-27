ecf6bab2b974b95bb523e5aca75538b76c52b7d5
Dima Menetro <kinoru11@gmail.com>
2026-01-27 11:33:56 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,10 +1,12 @@
 import NewQuery from './pages/NewQuery';
 import Results from './pages/Results';
+import History from './pages/History';
 
 
 export const PAGES = {
     "NewQuery": NewQuery,
     "Results": Results,
+    "History": History,
 }
 
 export const pagesConfig = {
