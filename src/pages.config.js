5dd001aab001a6f0ef0e03cbe7833b5fb6492f33
Dima Menetro <kinoru11@gmail.com>
2026-01-27 15:32:16 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,15 +1,15 @@
-import Diagnostics from './pages/Diagnostics';
 import History from './pages/History';
 import NewQuery from './pages/NewQuery';
 import Results from './pages/Results';
+import Diagnostics from './pages/Diagnostics';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Diagnostics": Diagnostics,
     "History": History,
     "NewQuery": NewQuery,
     "Results": Results,
+    "Diagnostics": Diagnostics,
 }
 
 export const pagesConfig = {
